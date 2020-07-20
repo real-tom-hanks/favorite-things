9600952db9038b21f5ad698a4c7e38226e1f0ba8
real-tom-hanks <lobstersurprises@gmail.com>
2020-07-19 20:56:03 -0700

    initial built app

---
+++ precache-manifest.5ff779050844767a32fe85943a5d8f4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a702d95c3d84e307cf6b09515de0c51f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be9f6938568827605284",
+    "url": "/static/css/main.af754c7a.chunk.css"
+  },
+  {
+    "revision": "2c1bb5b2d00ea1d613ae",
+    "url": "/static/js/2.060c4724.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.060c4724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9f6938568827605284",
+    "url": "/static/js/main.6bf8d3ed.chunk.js"
+  },
+  {
+    "revision": "a30cbad6c0332059dfc4",
+    "url": "/static/js/runtime-main.9061a63f.js"
+  }
+]);
